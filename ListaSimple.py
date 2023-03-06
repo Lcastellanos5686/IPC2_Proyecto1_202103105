3cb9e5951106d83cdd031cc293df47b654a59ff4
LuisCastellanos <luisda.betancourt@gmail.com>
2023-03-05 23:47:46 -0600

    Anadiendo logica celdas, tabla mejorada, falta solo menu

--- ListaSimple.py
+++ ListaSimple.py
@@ -39,14 +39,13 @@ class ListaSimple:
         filaActual = 501
         while n.nodo_siguiente is not None:
             if filaActual != n.celda.pos_y:
-                print("")
-                print("\ny = " + str(n.celda.pos_y))
+                ##print("")
+                ##print("\ny = " + str(n.celda.pos_y))
                 filaActual = n.celda.pos_y
-            ##print(str(n.celda.pos_x) + ", ", end="")
-            if n.celda.viva:
-                print(str(n.celda.id_organismo) + ", ", end="")
-            else:
-                print('-' + ", ", end="")
+            ##if n.celda.viva:
+                ##print(str(n.celda.id_organismo) + ", ", end="")
+            ##else:
+                ##print('-' + ", ", end="")
             
             n = n.nodo_siguiente
         print("")
@@ -54,8 +53,6 @@ class ListaSimple:
     def seReproduceLinea(self, x, y):
 
         #Primero asigna a CeldaBase la celda que se busca ver si sobrevive
-        y=y-1
-        x=x-1
         CeldaBase = self.primer_nodo
         while CeldaBase.nodo_siguiente is not None:
             if CeldaBase.celda.pos_x == x and CeldaBase.celda.pos_y == y:
@@ -100,8 +97,8 @@ class ListaSimple:
             if n.celda.pos_x == x and n.celda.id_organismo == CeldaBase.celda.id_organismo and n.celda.pos_y > y:
                 TopeInferior = n.celda.pos_y
             n = n.nodo_siguiente
-        print(TopeSuperior)
-        print(TopeInferior)
+        ##print(TopeSuperior)
+        ##print(TopeInferior)
 
         if TopeSuperior is None:
             TopeSuperior = CeldaBase.celda.pos_y
@@ -120,8 +117,181 @@ class ListaSimple:
 
             n = n.nodo_siguiente
 
-    def GraphvizParaTabla(): ##Generar tabla concatendando |'s. Recordar anadir una fila y columna extra
-        pass
+    def seReproduceDiagonal4(self, x, y, id): #tiene que salir 69, ya nos reconoce la primera diagonal siguiente, ahora hay que reconocer recursivamente
+        n = self.primer_nodo
+
+        y_tope = x
+        x_tope = y
+        while n.nodo_siguiente is not None:
+            if y_tope + 1 == n.celda.pos_y and x_tope + 1 == n.celda.pos_x:
+                if n.celda.id_organismo == id:
+                    ##print('se encontro tope en x= ' +  str(x_tope+1) + ", y= " + str(y_tope+1))
+                    Coordenadas = [x_tope+1, y_tope+1]
+                    return Coordenadas
+                else:
+                    return self.seReproduceDiagonal4(x_tope+1, y_tope+1, id)
+            n=n.nodo_siguiente
+    
+    def ReproducirDiagonal4(self, x_base, y_base, id, coordenadas):
+        n = self.primer_nodo
+        if coordenadas is None:
+            print("No hay ninguna wea en la diagonal pu")
+            return 
+        while n.nodo_siguiente is not None:
+            if y_base + 1 == n.celda.pos_y and x_base + 1 == n.celda.pos_x:
+
+                if n.celda.pos_x == coordenadas[0] and n.celda.pos_y == coordenadas[1]:
+                    n.celda.id_organismo = id
+                    return
+                else:
+                    n.celda.id_organismo = id
+                    self.ReproducirDiagonal4(x_base + 1,y_base +1, id,coordenadas)
+            n=n.nodo_siguiente
+    
+    def seReproduceDiagonal1(self, x, y, id): 
+        n = self.primer_nodo
+        y_tope = y
+        x_tope = x
+        while n.nodo_siguiente is not None:
+            if y_tope - 1 == n.celda.pos_y and x_tope + 1 == n.celda.pos_x: ##si la celda seleccionada tiene las mismas coordenadas que la celda siguiente a la base
+                
+                if n.celda.id_organismo == id:
+                    Coordenadas = [x_tope+1, y_tope-1]
+                    return Coordenadas
+                else:
+                    return self.seReproduceDiagonal1(x_tope+1, y_tope-1, id)
+            n=n.nodo_siguiente
+    
+    def ReproducirDiagonal1(self, x_base, y_base, id, coordenadas):
+        n = self.primer_nodo
+        if coordenadas is None:
+            return 
+        print(coordenadas)
+        while n.nodo_siguiente is not None:
+            if y_base - 1 == n.celda.pos_y and x_base + 1 == n.celda.pos_x:
+                if n.celda.pos_x == coordenadas[0] and n.celda.pos_y == coordenadas[1]:
+                    n.celda.id_organismo = id
+                    print('entro a cambiar de id y return')
+                    return
+                else:
+                    print('entro a cambiar de id')
+                    n.celda.id_organismo = id
+                    self.ReproducirDiagonal1(x_base + 1,y_base -1, id,coordenadas)
+            n=n.nodo_siguiente
+        
+
+    def seReproduceDiagonal2(self, x, y, id): 
+        n = self.primer_nodo
+        y_tope = y
+        x_tope = x
+        while n.nodo_siguiente is not None:
+            if y_tope - 1 == n.celda.pos_y and x_tope - 1 == n.celda.pos_x: ##si la celda seleccionada tiene las mismas coordenadas que la celda siguiente a la base
+                
+                if n.celda.id_organismo == id:
+                    Coordenadas = [x_tope-1, y_tope-1]
+                    return Coordenadas
+                else:
+                    return self.seReproduceDiagonal2(x_tope-1, y_tope-1, id)
+            n=n.nodo_siguiente
+    
+    def ReproducirDiagonal2(self, x_base, y_base, id, coordenadas):
+        n = self.primer_nodo
+        if coordenadas is None:
+            return 
+        print(coordenadas)
+        while n.nodo_siguiente is not None:
+            if y_base - 1 == n.celda.pos_y and x_base - 1 == n.celda.pos_x:
+                if n.celda.pos_x == coordenadas[0] and n.celda.pos_y == coordenadas[1]:
+                    n.celda.id_organismo = id
+                    print('entro a cambiar de id y return')
+                    return
+                else:
+                    print('entro a cambiar de id')
+                    n.celda.id_organismo = id
+                    self.ReproducirDiagonal2(x_base - 1,y_base -1, id,coordenadas)
+            n=n.nodo_siguiente
+        
+
+    def seReproduceDiagonal3(self, x, y, id): 
+        n = self.primer_nodo
+        y_tope = y
+        x_tope = x
+        while n.nodo_siguiente is not None:
+            if y_tope + 1 == n.celda.pos_y and x_tope - 1 == n.celda.pos_x: ##si la celda seleccionada tiene las mismas coordenadas que la celda siguiente a la base
+                
+                if n.celda.id_organismo == id:
+                    Coordenadas = [x_tope-1, y_tope+1]
+                    return Coordenadas
+                else:
+                    return self.seReproduceDiagonal3(x_tope-1, y_tope+1, id)
+            n=n.nodo_siguiente
+    
+    def ReproducirDiagonal3(self, x_base, y_base, id, coordenadas):
+        n = self.primer_nodo
+        if coordenadas is None:
+            return 
+        print(coordenadas)
+        while n.nodo_siguiente is not None:
+            if y_base + 1 == n.celda.pos_y and x_base - 1 == n.celda.pos_x:
+                if n.celda.pos_x == coordenadas[0] and n.celda.pos_y == coordenadas[1]:
+                    n.celda.id_organismo = id
+                    print('entro a cambiar de id y return')
+                    return
+                else:
+                    print('entro a cambiar de id')
+                    n.celda.id_organismo = id
+                    self.ReproducirDiagonal3(x_base - 1,y_base +1, id,coordenadas)
+            n=n.nodo_siguiente
+
+    def GraphvizParaTabla(self) -> str: ##Generar tabla concatendando |'s. Recordar anadir una fila y columna extra
+        contador=-1
+        Texto = ""
+        n = self.primer_nodo
+        Fila = n.celda.pos_y
+        m = self.primer_nodo
+        while n.nodo_siguiente is not None:
+            #Anade la fila inicial
+            if contador == -1:
+                Texto = "{x y|"
+                while m.nodo_siguiente is not None:
+                    if m.celda.pos_y != 0:
+                        break
+                    if m.celda.pos_x < 10:
+                        Texto += str(m.celda.pos_x) + "."
+                    else:
+                        Texto += str(m.celda.pos_x)
+                    m = m.nodo_siguiente
+                    Texto += "|"
+                Texto = Texto[:len(Texto)-1]
+                Texto += "}|\n{0 .|"
+                
+            #Anade celdas
+            if contador != -1:
+                #Anade celdas vivas
+                if Fila != n.celda.pos_y:
+                    Texto = Texto[:len(Texto)-1]
+                    if n.celda.pos_y < 10:
+                        Texto+="}|\n{" + str(n.celda.pos_y) + " .|"
+                    else:
+                        Texto+="}|\n{" + str(n.celda.pos_y) + ".|"
+                    Fila= n.celda.pos_y
+                
+                if n.celda.id_organismo == None:
+                    Texto += "--|"
+                else: 
+                    if n.celda.id_organismo < 10:
+                        Texto += str(n.celda.id_organismo) + ".|"
+                    else:
+                        Texto += str(n.celda.id_organismo) + "|"
+                n = n.nodo_siguiente
+            contador += 1
+        Texto = Texto[:len(Texto)-1]
+        Texto += "}"
+        ##print(Texto)
+        return Texto
+
+
+
 
 
 
--- Proyecto1.py
+++ Proyecto1.py
@@ -2,6 +2,7 @@ from ListaSimple import*
 from xml.dom import minidom
 from tkinter.filedialog import askopenfilename
 from random import*
+import os
 
 class Celda:
     def __init__(self, pos_x, pos_y):
@@ -25,57 +26,115 @@ def crearCuadricula(x,y,Lista):    ## empiezan de 1
             else:
                 Lista.agregar_celda(Celda(j,i))
     
-    Lista.agregar_celda(Celda(x-1,y-1))
+    Lista.agregar_celda(Celda(x,y))
 
 def anadirOrganismos(x, y, ID, Lista):
     n = Lista.primer_nodo
-    x=x-1
-    y=y-1
     while n.nodo_siguiente is not None:
         if n.celda.pos_x == x and n.celda.pos_y == y:
             break
         n = n.nodo_siguiente
     n.celda.darVida(ID, True)
 
-
-
 def GenerarGrafica(Lista):
     docGraphviz = "digraph structs {\n"
     docGraphviz += "    node [shape=record];\n"
-    docGraphviz += "    MATRIZ [\n"
-    docGraphviz += "        label=\"\n"
+    docGraphviz += "    rankdir = LR"
+    docGraphviz += "    MATRIZ [fontname = \"Courier New\",\n"
+    docGraphviz += "        label=\"\n" + Lista.GraphvizParaTabla()
+    docGraphviz += "\"]"
+    docGraphviz += "\n                }"
+    print(docGraphviz)
+    archivoDOT = open("muestra.dot","w")
+    archivoDOT.write(docGraphviz)
+    archivoDOT.close()
+    os.system("dot.exe -Tpng muestra.dot -o  muestra.png")
 
+def LeerXml(Lista):
+    filename = askopenfilename()
+    objetoXML = minidom.parse(filename)
 
+    coleccionX  = objetoXML.getElementsByTagName('columnas')
+    coleccionY  = objetoXML.getElementsByTagName('filas')
+    muestra     = objetoXML.getElementsByTagName('muestra')
 
-    
-    
+    dimensionX = coleccionX[0].childNodes[0].data
+    dimensionY = coleccionY[0].childNodes[0].data
 
+    crearCuadricula(int(dimensionX), int(dimensionY), Lista)
+    organismosXML = objetoXML.getElementsByTagName('organismo')
+    celdasVivasXML = objetoXML.getElementsByTagName('celdaViva')
+    print('         || Datos de la Muestra||\n')
+    contador=1
+    for organismo in organismosXML:
 
+        codigo = organismo.childNodes[1].firstChild.data
+        nombre = organismo.childNodes[3].firstChild.data
+        
+        print('codigo: ' + codigo)
+        print('nombre: ' + nombre + "\n")
+        ##nuevoOrganismo = Organismo(codigo,nombre,letra)
+        ##nuevaMuestra.listaOrganismos.agregar_al_inicio(nuevoOrganismo)
+        ##letra = letra + 1
+        print('     Se encuentra en:')
+        for celdaViva in celdasVivasXML:
+            fila            = celdaViva.childNodes[1].firstChild.data
+            columna         = celdaViva.childNodes[3].firstChild.data
+            codigoOrganismo = celdaViva.childNodes[5].firstChild.data
+            if codigoOrganismo == codigo:
+                print('el contador esta en ' + str(contador))
+                anadirOrganismos(int(columna)+1, int(fila)+1, contador, Lista)
+                print('                         x=' + fila + ', y=' + columna)
+        contador +=1
 
 ##Ejecucion principal!!!!!!!!!!!!!!!!!!!!!1
 
 
 ListaCeldas = ListaSimple()
-
 crearCuadricula(14, 18, ListaCeldas)
-
-ListaCeldas.imprimeEnConsola()
-
-#organismo de prueba 2
-for i in range(1,15):
-    anadirOrganismos(i,5,2,ListaCeldas)
-
-for i in range(1,19):
-    anadirOrganismos(5,i,2,ListaCeldas)
-
+##ListaCeldas.imprimeEnConsola()
+#organismo de prueba 
+for i in range(0,14):
+    anadirOrganismos(i,4,2,ListaCeldas)
+for i in range(0,18):
+    anadirOrganismos(4,i,2,ListaCeldas)
+for i in range(0,14):
+    contador=4
+    anadirOrganismos(i,i,2,ListaCeldas)
+for i in range(0,9):
+    contador=4
+    anadirOrganismos(8-i,i,2,ListaCeldas)
 #organismo de prueba 1
-anadirOrganismos(5,5,1,ListaCeldas) ## Base
-anadirOrganismos(5,2,1,ListaCeldas) ## Tope Superior
-anadirOrganismos(5,9,1,ListaCeldas) ## Tope Inferior
-anadirOrganismos(2,5,1,ListaCeldas) ## Tope Anterior
-anadirOrganismos(9,5,1,ListaCeldas) ## Tope Posterior
-ListaCeldas.seReproduceLinea(5,5)
-ListaCeldas.revisarVidas()
+
+anadirOrganismos(4,4,1,ListaCeldas) ## Base
+anadirOrganismos(4,1,1,ListaCeldas) ## Tope Superior
+anadirOrganismos(4,8,1,ListaCeldas) ## Tope Inferior
+anadirOrganismos(1,4,1,ListaCeldas) ## Tope Anterior
+anadirOrganismos(8,4,1,ListaCeldas) ## Tope Posterior
+anadirOrganismos(10,10,1,ListaCeldas)
+anadirOrganismos(1,1,1,ListaCeldas)
+anadirOrganismos(5,5,69,ListaCeldas)
+anadirOrganismos(2,6,1,ListaCeldas)
+anadirOrganismos(7,1,1,ListaCeldas)
+ListaCeldas.seReproduceLinea(4,4)
+
+ListaCeldas.ReproducirDiagonal4(4,4,1,ListaCeldas.seReproduceDiagonal4(4,4,1))
+ListaCeldas.ReproducirDiagonal1(4,4,1,ListaCeldas.seReproduceDiagonal1(4,4,1))
+ListaCeldas.ReproducirDiagonal2(4,4,1,ListaCeldas.seReproduceDiagonal2(4,4,1))
+ListaCeldas.ReproducirDiagonal3(4,4,1,ListaCeldas.seReproduceDiagonal3(4,4,1))
+anadirOrganismos(5,7,69,ListaCeldas)
+##ListaCeldas.revisarVidas()
+##ListaCeldas.GraphvizParaTabla()
+GenerarGrafica(ListaCeldas)
+
+
+##ListaCeldas = ListaSimple()
+
+##LeerXml(ListaCeldas)
+##GenerarGrafica(ListaCeldas)
+
+
+
 
 
 
